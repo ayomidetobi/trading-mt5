dee416d3204cce35d26f7e4859a423a7a798e6bb
ALASHI1 <alashimuyiwa@gmail.com>
2022-08-24 18:22:54 +0100

    thread

--- main.py
+++ main.py
@@ -96,12 +96,13 @@ def get_new_tab():
 
     # starting service for chrome web driver
     service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
-    service = Service(executable_path=ChromeDriverManager().install())
+    # service = Service(executable_path=ChromeDriverManager().install())
     # initiating chrome web driver
     web_browser = webdriver.Chrome(
         options=opts,
         service=service,
     )
+
     return web_browser
 
 
